f78c8ff40c941e8212ad90d29baa17dc025eaeab
saravia <tecla253@gmail.com>
2015-09-21 23:25:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/saravia/laboratorio43_ejercicio1/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.saravia.laboratorio43_ejercicio1;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private Handler handler;
+    private ProgressBar progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progress = (ProgressBar) findViewById(R.id.progressBar1);
+        handler = new Handler();
+    }
+
+    public void startProgress(View view) {
+        // Do something long
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 10; i++) {
+                    final int value = i;
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progress.setProgress(value);
+                        }
+                    });
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
